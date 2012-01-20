4d326fe2c2cb46332e8f6e1fea7432b0d9b308ec
cmp@chromium.org <cmp@chromium.org@4ff67af0-8c30-449e-8e8b-ad334ec8d88c>
2012-01-20 19:46:50 +0000

    Add deps2git.
    R=nsylvain@chromium.org
    
    Review URL: http://codereview.chromium.org/9195025
    
    git-svn-id: http://src.chromium.org/svn/trunk/tools/deps2git@118467 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

---
+++ deps2git.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+import optparse
+import sys
+
+
+import deps_utils
+
+
+def SplitSvnUrl(url):
+  """Given a SVN URL, return a set containing the URL and the revision."""
+  url_split = url.split('@')
+  svn_url = url_split[0]
+  svn_rev = 'HEAD'
+  if len(url_split) == 2:
+    svn_rev = url_split[1]
+  return (svn_url, svn_rev)
+
+
+def SvnRevToGitHash(svn_rev, git_url, repos_path, git_host):
+  """Convert a SVN revision to a Git commit id."""
+  git_repo = None
+  if git_url.startswith(git_host):
+    git_repo = git_url.replace(git_host, '')
+  else:
+    raise Exception('Unknown git server')
+  if repos_path is None:
+    # We're running without a repository directory (i.e. no -r option).
+    # We cannot actually find the commit id, but this mode is useful
+    # just for testing the URL mappings.  Produce an output file that
+    # can't actually be used, but can be eyeballed for correct URLs.
+    return ('xxx-r%s' % svn_rev)
+  # TODO(unknown_coder): Most of the errors happen when people add new repos
+  # that actually matches one of our expressions but dont exist yet on
+  # git.chromium.org.  We should probably at least ping git_url to make sure it
+  # exists.
+  git_repo_path = os.path.join(repos_path, git_repo)
+  if not os.path.exists(git_repo_path):
+    git_tools.Clone(git_url, git_repo_path)
+  git_tools.Fetch(git_repo_path)
+  return git_tools.Search(git_repo_path, svn_rev)
+
+
+def ConvertDepsToGit(deps, repos, deps_type, vars)
+  """Convert a 'deps' section in a DEPS file from SVN to Git."""
+  new_deps = {}
+  deps_module = os.path.join(os.path.dirname(__file__),
+                             'svn_to_git_%s' % deps_type)
+  if not os.path.exists(depos_module):
+    raise Exception('invalid DEPS type')
+  svn_to_git = __import__(deps_module)
+
+  for dep in deps:
+    # Get the SVN URL and the SVN rev for this dep.
+    (svn_url, svn_rev) = SplitSvnUrl(deps[dep])
+
+    # Convert this SVN URL to a Git URL.
+    (path, git_url) = svn_to_git.SvnUrlToGitUrl(dep, svn_url)
+
+    if not path or not git_url:
+      # We skip this path, this must not be required with Git.
+      continue
+
+    # Get the Git hash based off the SVN rev.
+    git_hash = ''
+    if svn_rev != 'HEAD':
+      git_hash = '@%s' % svn_to_git.SvnRevToGitHash(svn_rev, git_url, repos,
+          svn_to_git.GIT_HOST)
+
+    # If this is webkit, we need to add the var for the hash.
+    if dep == 'src/third_party/WebKit/Source':
+      vars['webkit_rev'] = git_hash
+      git_hash = 'VAR_WEBKIT_REV'
+
+    # Add this Git dep to the new deps.
+    new_deps[path] = '%s%s' % (git_url, git_hash)
+
+  return new_deps
+
+
+def main():
+  parser = optparse.OptionParser()
+  parser.add_option('-d', '--deps',
+                    help='path to the DEPS file to convert')
+  parser.add_option('-o', '--out',
+                    help='path to the converted DEPS file')
+  parser.add_option('-t', '--type', default='public',
+                    help='type of DEPS file (public, etc)')
+  parser.add_option('-r', '--repos',
+                    help='path to the directory holding all the Git repos')
+  options, args = parser.parse_args()
+
+  # Get the content of the DEPS file.
+  deps_content =  deps_utils.GetDepsContent(options.deps)
+  (deps, deps_os, include_rules, skip_child_includes, hooks) = deps_content
+
+  # Create a var containing the Git and Webkit URL, this will make it easy for
+  # people to use a mirror instead.
+  vars = {'git_url': 'http://git.chromium.org',
+          'webkit_url': 'http://git.chromium.org/external/WebKit_trimmed.git'}
+
+  # Convert the DEPS file to Git.
+  deps = ConvertDepsToGit(deps, options.repos, options.type, vars)
+  for os_dep in deps_os:
+    deps_os[os_dep] = ConvertDepsToGit(deps_os[os_dep], options.repos,
+                                       options.type, vars)
+
+  # Write the DEPS file to disk.
+  deps_utils.WriteDeps(options.out, vars, deps, deps_os, include_rules,
+                       skip_child_includes, hooks)
+  return 0
+
+if '__main__' == __name__:
+  sys.exit(main())
---
+++ svn_to_git_public.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+import os
+import re
+
+
+import git_tools
+
+
+GIT_HOST = 'http://git.chromium.org/'
+
+
+def SvnUrlToGitUrl(path, svn_url):
+  """Convert a chromium SVN URL to a chromium Git URL."""
+
+  match = re.match('http://src.chromium.org/svn(/.*)', svn_url)
+  if match:
+    svn_url = match.group(1)
+
+  # A few special cases.
+  if svn_url == '/trunk/deps/page_cycler/acid3':
+    return (path, 'http://git.chromium.org/chromium/deps/acid3.git')
+
+  if svn_url == '/trunk/deps/gpu/software_rendering_list':
+    return (path, 'http://git.chromium.org/chromium/deps/gpu/software_rendering_list.git')
+
+  if svn_url == '/trunk/tools/third_party/python_26':
+    return (path, 'http://git.chromium.org/chromium/deps/python_26.git')
+
+  if svn_url == '/trunk/deps/support':
+    return (path, 'http://git.chromium.org/chromium/support.git')
+
+  if svn_url == '/trunk/deps/frame_rate/content':
+    return (path, 'http://git.chromium.org/chromium/frame_rate/content.git')
+
+  if svn_url == 'svn://svn.chromium.org/jsoncpp/trunk/jsoncpp':
+    return (path, 'http://git.chromium.org/external/jsoncpp/jsoncpp.git')
+
+  if svn_url in ('http://selenium.googlecode.com/svn/trunk/py/test',
+                 '/trunk/deps/reference_builds/chrome'):
+    # Those can't be git svn cloned. Skipping for now.
+    return (None, None)
+
+  # Projects on sourceforge using trunk
+  match = re.match('http?://(.*).svn.sourceforge.net/svnroot/(.*)/trunk(.*)', svn_url)
+  if match:
+    repo = '%s%s.git' % (match.group(2), match.group(3))
+    return (path, 'http://git.chromium.org/external/%s' % repo)
+
+  # Projects on googlecode.com using trunk.
+  match = re.match('http?://(.*).googlecode.com/svn/trunk(.*)', svn_url)
+  if match:
+    repo = '%s%s.git' % (match.group(1), match.group(2))
+    return (path, 'http://git.chromium.org/external/%s' % repo)
+
+  # Projects on googlecode.com usng branches.
+  match = re.match('http://(.*).googlecode.com/svn/branches/(.*)', svn_url)
+  if match:
+    repo = '%s/%s.git' % (match.group(1), match.group(2))
+    return (path, 'http://git.chromium.org/external/%s' % repo)
+
+  # Projects that are subdirectories of the native_client repository.
+  match = re.match('http://src.chromium.org/native_client/trunk/(.*)', svn_url)
+  if match:
+    repo = '%s.git' % match.group(1)
+    return (path, 'http://git.chromium.org/native_client/%s' % repo)
+
+  # Projects that are subdirectories of the chromium/{src,tools} repository.
+  match = re.match('/trunk/((src|tools)/.*)', svn_url)
+  if match:
+    repo = '%s.git' % match.group(1)
+    return (path, 'http://git.chromium.org/chromium/%s' % repo)
+
+  # Main webkit directory.
+  if svn_url == 'http://svn.webkit.org/repository/webkit/trunk/Source':
+    return ('src/third_party/WebKit',
+            'http://git.chromium.org/external/WebKit_trimmed.git')
+
+  # Ignore all webkit directories, since we fetch the whole thing directly.
+  if svn_url == '/trunk/deps/third_party/WebKit':
+    return (None, None)
+
+  if svn_url.startswith('http://svn.webkit.org'):
+    return (None, None)
+
+  # Subdirectories of the chromium deps/third_party directory.
+  match = re.match('/trunk/deps/third_party/(.*)', svn_url)
+  if match:
+    repo = '%s.git' % match.group(1)
+    return (path, 'http://git.chromium.org/chromium/deps/%s' % repo)
+
+  # Subdirectories of the chromium deps/reference_builds directory.
+  match = re.match('/trunk/deps/reference_builds/(.*)', svn_url)
+  if match:
+    repo = '%s.git' % match.group(1)
+    return (path, 'http://git.chromium.org/chromium/reference_builds/%s' % repo)
+
+  # Nothing yet? Oops.
+  print "No match for %s" % svn_url
